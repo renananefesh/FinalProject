5459d07d1c7afec154626e4a3298f381dc71cd40
talyast <53152988+talyast@users.noreply.github.com>
2020-05-01 13:11:30 +0300

    write to file. cant read from file - in LoginPage

--- app/src/main/java/com/example/project/LoginPage.java
+++ app/src/main/java/com/example/project/LoginPage.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -24,30 +26,59 @@ import java.util.logging.Logger;
 
 public class LoginPage extends AppCompatActivity {
     private Button login_button, signin_button;
-EditText username, password;
-String filename = "test.txt";
+    EditText username, password;
+    String filename = "test.txt";
+    String text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         login_button = (Button) findViewById(R.id.loginbutton);
+        username =(EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openUserPage();
-                username =(EditText) findViewById(R.id.username);
-                password = (EditText) findViewById(R.id.password);
-              FileOutputStream fileOutputStream;
-            String s =  username.getText().toString();
-                try {
-                    fileOutputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-                    fileOutputStream.write(s.getBytes());
-                    fileOutputStream.write(5);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (filename.toString() != null && filename.trim() != "") {
+                    FileInputStream fileInputStream = null;
+                    ///////////////////////////////////////////////////////////////////////////
+                    InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                    //////////////////////////////////////////////////////////////////////////
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    try {
+                        fileInputStream = openFileInput(filename);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        while ((text = bufferedReader.readLine()) != null) {
+                            stringBuilder.append(text);
+                            if(text ==username.getText().toString())
+                                openUserPage();
+                        }
+                    } catch (Exception e) {
+                        Toast.makeText(LoginPage.this, "no such user, please sign up ", Toast.LENGTH_LONG).show();
+                    }
+
+
                 }
             }
+
         });
+
+//              FileOutputStream fileOutputStream;
+//            String s =  username.getText().toString();
+//                try {
+//                    fileOutputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+//                    fileOutputStream.write(s.getBytes());
+//                    fileOutputStream.write(5);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+
 
         signin_button = (Button) findViewById(R.id.signinbutton);
         signin_button.setOnClickListener(new View.OnClickListener() {
@@ -56,11 +87,8 @@ String filename = "test.txt";
                 openNewUserPage();
             }
         });
+    };
 
-
-
-
-    }
     public void openUserPage() {
         Intent intent = new Intent(this, UserEvents.class);
         startActivity(intent);
